82f1bc87461030d98bed3ffe935bc295db99adc2
Mohit Singh <mohitsinghx4@gmail.com>
2024-08-25 13:50:00 +0530

    last - commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,73 +19,61 @@ export const Projects = () => {
        <div className="portfolios">
         <div className="portfolio-item">
           <div className="imagee">
-            <img src="\img\nature.jpg" alt="image" />
+            <img src="\img\cartopia-img.png" alt="image" />
           </div>
           <div className="hover-item"> 
-            <h3>Project source</h3>
+            <h3  style={{fontWeight:"bold" , color:"black"}}>Cartopia :<span style={{ display:"inline-block" ,backgroundColor:"white" , fontWeight:"bold", borderRadius:"10px" , padding:"5px"}}> Ecommerce shopping website</span></h3>
             <div className="icons">
-              <a href="http://www.google.com" target="_blank">< FaGithub color='black' size={35}/></a>
-              <a href="http://" target="_blank"><FaLink color='black' size={35}/></a>
+              <a href="https://github.com/mohitsinghgarry/Ecommerce-website-cartopia" target="_blank">< FaGithub color='black' size={35}/></a>
+              <a href="https://cartopiaecommerce.netlify.app/" target="_blank"><FaLink color='black' size={35}/></a>
             </div>
           </div>
         </div>
         <div className="portfolio-item">
           <div className="imagee">
-            <img src="\img\nature.jpg" alt="image" />
+            <img src="\img\weather-img.png" alt="image" />
           </div>
           <div className="hover-item"> 
-            <h3>Project source</h3>
+            <h3  style={{fontWeight:"bold" , color:"black"}}>Weatherify : <span style={{ display:"inline-block" ,backgroundColor:"white" , fontWeight:"bold", borderRadius:"10px" , padding:"5px"}} > personalized weather webapp</span></h3>
             <div className="icons">
-              <a href="http://www.google.com" target="_blank">< FaGithub color='black' size={35}/></a>
-              <a href="http://" target="_blank"><FaLink color='black' size={35}/></a>
+              <a href="https://github.com/mohitsinghgarry/weather-backend-new" target="_blank">< FaGithub color='black' size={35}/></a>
+              <a href="https://weather-backend-new.onrender.com/" target="_blank"><FaLink color='black' size={35}/></a>
             </div>
           </div>
         </div>
         <div className="portfolio-item">
           <div className="imagee">
-            <img src="\img\nature.jpg" alt="image" />
+            <img src="\img\jobportal.png" alt="image" />
           </div>
           <div className="hover-item"> 
-            <h3>Project source</h3>
+            <h3 style={{fontWeight:"bold" , color:"black"}}>Opportunity Orbit :<span style={{ display:"inline-block" ,backgroundColor:"white" , fontWeight:"bold", borderRadius:"10px" , padding:"5px"}}>Job Portal</span></h3>
             <div className="icons">
-              <a href="http://www.google.com" target="_blank">< FaGithub color='black' size={35}/></a>
-              <a href="http://" target="_blank"><FaLink color='black' size={35}/></a>
+              <a href="https://github.com/mohitsinghgarry/JOB_PORTAL_FRONTEND" target="_blank">< FaGithub color='black' size={35}/></a>
+              <a href="https://opportunityorbitjobportal.netlify.app/" target="_blank"><FaLink color='black' size={35}/></a>
             </div>
           </div>
         </div>
         <div className="portfolio-item">
           <div className="imagee">
-            <img src="\img\nature.jpg" alt="image" />
+            <img src="\img\tourhub.png" alt="image" />
           </div>
           <div className="hover-item"> 
-            <h3>Project source</h3>
+            <h3 style={{fontWeight:"bold" , color:"black"}}>Tour Hub :<span style={{ display:"inline-block" ,backgroundColor:"white" , fontWeight:"bold", borderRadius:"10px" , padding:"5px"}}>Tour Cards demo</span></h3>
             <div className="icons">
-              <a href="http://www.google.com" target="_blank">< FaGithub color='black' size={35}/></a>
-              <a href="http://" target="_blank"><FaLink color='black' size={35}/></a>
+              <a href="https://github.com/mohitsinghgarry/tourist-web-react" target="_blank">< FaGithub color='black' size={35}/></a>
+              <a href="https://tourist-web-mohit.netlify.app/" target="_blank"><FaLink color='black' size={35}/></a>
             </div>
           </div>
         </div>
         <div className="portfolio-item">
           <div className="imagee">
-            <img src="\img\nature.jpg" alt="image" />
+            <img src="\img\Counterapp.png" alt="image" />
           </div>
           <div className="hover-item"> 
-            <h3>Project source</h3>
+            <h3 style={{fontWeight:"bold" , color:"black"}}>Counter <span style={{ display:"inline-block" ,backgroundColor:"white" , fontWeight:"bold", borderRadius:"10px" , padding:"5px"}}>App</span></h3>
             <div className="icons">
-              <a href="http://www.google.com" target="_blank">< FaGithub color='black' size={35}/></a>
-              <a href="http://" target="_blank"><FaLink color='black' size={35}/></a>
-            </div>
-          </div>
-        </div>
-        <div className="portfolio-item">
-          <div className="imagee">
-            <img src="\img\nature.jpg" alt="image" />
-          </div>
-          <div className="hover-item"> 
-            <h3>Project source</h3>
-            <div className="icons">
-              <a href="http://www.google.com" target="_blank">< FaGithub color='black' size={35}/></a>
-              <a href="http://" target="_blank"><FaLink color='black' size={35}/></a>
+              <a href="https://github.com/mohitsinghgarry/Counter-app-using-react" target="_blank">< FaGithub color='black' size={35}/></a>
+              <a href="https://counter-app-mohit.netlify.app/" target="_blank"><FaLink color='black' size={35}/></a>
             </div>
           </div>
         </div>
